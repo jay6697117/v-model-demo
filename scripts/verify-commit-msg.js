6efcffa42404a49f7984b60cd10e4895c8b9a671
zhangjinhui <871506263@qq.com>
2021-08-10 17:59:10 +0800

    006

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -1,12 +1,12 @@
-const chalk = require('chalk')
-const msgPath = process.env.GIT_PARAMS
-const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
+const chalk = require('chalk');
+const msgPath = process.env.GIT_PARAMS;
+const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 
 const commitRE =
-  /^((?<!([A-Z]{1,10})-?)[A-Z]+-\d+): \[(feature|fix|docs|style|refactor|test|chore)\] [A-Za-z0-9|\s]{1,80}$/
+  /^((?<!([A-Z]{1,10})-?)[A-Z]+-\d+): \[(feature|fix|docs|style|refactor|test|chore)\] [A-Za-z0-9|\s]{1,80}$/;
 
 if (!commitRE.test(msg)) {
-  console.log()
+  console.log();
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n` +
       chalk.red('  Proper commit message format is required for automated changelog generation. Examples:\n\n') +
@@ -14,6 +14,6 @@ if (!commitRE.test(msg)) {
       `    ${chalk.green('fix(v-model): handle events on blur (close #28)')}\n\n` +
       chalk.red('  See .gitlab/COMMIT_CONVENTION.md for more details.\n') +
       chalk.red(`  You can also use ${chalk.cyan('git cz')} to interactively generate a commit message.\n`)
-  )
-  process.exit(1)
+  );
+  process.exit(1);
 }
